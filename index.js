b56804a348045adaefac19457ff479dba9c99ecf
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-03-01 12:07:55 +0600

    Done...

--- index.js
+++ index.js
@@ -7,18 +7,22 @@ const loadMeals = (searchText) => {
 };
 
 const displayMeals = (meals) => {
-  for (let i = 0; i < 6; i++) {
+  const foodContainer = document.getElementById("food-container");
+  foodContainer.innerText = "";
+  
+  for (let i = 0; i < 6 ; i++) {
     const meal = meals[i];
 
     // console.log(meal);
-
-    const foodContainer = document.getElementById("food-container");
 
     const foodDiv = document.createElement("div");
     foodDiv.innerHTML = `
 
     <div class="card card-side bg-white shadow-xl text-black text-xl ">
-    <figure><img class="w-[40rem] h-[20rem]" src="${meal.strMealThumb}" /></figure>
+    <figure><img class="w-[40rem] h-[20rem]" src="${
+      meal.strMealThumb ? meal.strMealThumb : ""
+    }
+    " /></figure>
     <div class="card-body">
         <h2 class="card-title">${meal.strMeal}</h2>
         <p>There are many variations of passages of available, but the majority have suffered.</p>
@@ -32,11 +36,15 @@ const displayMeals = (meals) => {
     `;
 
     foodContainer.appendChild(foodDiv);
+    document.getElementById("show-btn").classList.remove("hidden");
   }
 };
 
-const loadMealsAll = (value) => {
-  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${value}`;
+const loadMealsAll = () => {
+  const searchValue = document.getElementById("input-field").value;
+  document.getElementById("input-field").value = "";
+
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`;
 
   fetch(url)
     .then((res) => res.json())
@@ -45,19 +53,24 @@ const loadMealsAll = (value) => {
 
 const displayMealsAll = (meals) => {
   console.log(meals);
+  const foodContainer = document.getElementById("food-container");
+
   for (let i = 6; i < meals.length; i++) {
     const meal = meals[i];
     console.log(meals);
     //     // document.getElementById("show-btn").classList.add("disabled")
     //     // console.log(meal);
 
-    const foodContainer = document.getElementById("food-container");
+    // foodContainer.innerHTML = "";
 
     const foodDiv = document.createElement("div");
     foodDiv.innerHTML = `
 
        <div class="card card-side bg-white shadow-xl text-black text-xl ">
-       <figure><img class="w-[40rem] h-[20rem]" src="${meal.strMealThumb}" /></figure>
+       <figure><img class="w-[40rem] h-[20rem]" src="${
+         meal.strMealThumb ? meal.strMealThumb : ""
+       }
+       " /></figure>
        <div class="card-body">
            <h2 class="card-title">${meal.strMeal}</h2>
            <p>There are many variations of passages of available, but the majority have suffered.</p>
@@ -76,10 +89,9 @@ const displayMealsAll = (meals) => {
 
 const searchMeals = () => {
   const searchValue = document.getElementById("input-field").value;
-  document.getElementById("input-field").value = "";
+  // document.getElementById("input-field").value = "";
 
   loadMeals(searchValue);
-  loadMealsAll(searchValue);
+  // loadMealsAll(searchValue);
   return searchValue;
 };
-
